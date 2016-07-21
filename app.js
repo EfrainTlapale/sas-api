a45445251b0e1c67b356ce772cfd11e30503c35d
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2016-07-21 10:25:24 -0500

    Se añadio la documentacion al root de la API

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/api', express.static(__dirname + '/doc/'));
 app.use('/api', require('./routes'));
 
 module.exports = app;
